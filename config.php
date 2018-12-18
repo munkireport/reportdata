ed9af74e8f9728e8c8eeeb8d576a5c1df9fd7bfc
Arjen van Bochoven <nejra@me.com>
2018-12-18 23:20:38 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-  'ip_config_path' => env('REPORTDATA_IP_CONFIG_PATH', APP_ROOT . 'local/module_configs/ip_ranges.yml'),
+  'ip_config_path' => env('REPORTDATA_IP_CONFIG_PATH', module_conf('ip_ranges.yml')),
 ];
