c4a04afb7315fdbfb8dfc58b34d96dba5751b7eb
Arjen van Bochoven <arjen@bochoven.net>
2020-05-11 23:43:27 +0200

    Use FakerData Store

--- reportdata_factory.php
+++ reportdata_factory.php
@@ -1,8 +1,23 @@
 <?php
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(Reportdata_model::class, function (Faker\Generator $faker) {
+
+    $machines = [
+        ['JYVX', 'Macmini', 'Macmini8,1', 'Mac mini (2018)'],
+        ['HDNK', 'MacBook', 'MacBook9,1', 'MacBook (Retina, 12-inch, Early 2016)'],
+        ['MD6N', 'MacBook Pro', 'MacBookPro16,1', 'MacBook Pro (16-inch, 2019)'],
+        ['H1DP', 'MacBook Pro', 'MacBookPro12,1', 'MacBook Pro (Retina, 13-inch, Early 2015)'],
+        ['J9X6', 'iMac', 'iMac18,3', 'iMac (Retina 5K, 27-inch, 2017)'],
+        ['JWDW', 'iMac', 'iMac19,2', 'iMac (Retina 4K, 21.5-inch, 2019)'],
+        ['P7QM', 'MacPro', 'MacPro7,1', 'Mac Pro (2019)'],
+    ];
+
+    FakerDataStore::add('machine', $faker->randomElement($machines));
+    list($modelcode, $machine_name, $machine_model, $machine_desc) = FakerDataStore::get('reportdata_factory', 'machine');
+
     return [
-        'serial_number' => $faker->unique()->regexify('[A-Z0-9]{12}'),
+        'serial_number' => $faker->unique()->regexify('[A-Z0-9]{3}[CDFGHJKLMNPQRSTVWXYZ][123456789CDFGHJKLMNPQRTVWXY][A-Z0-9]{3}') .$modelcode,
         'console_user' => $faker->userName,
         'long_username' => $faker->name,
         'remote_ip' => $faker->ipv4,
